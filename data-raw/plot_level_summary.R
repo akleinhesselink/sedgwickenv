5f99e13ad62472e152ee98f7d9568425c4b7e368
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2018-09-25 15:22:49 -0700

    remove outdated script

--- data-raw/plot_level_summary.R
+++
@@ -1,5 +0,0 @@
-file_list <- list()
-file_names <- list.files("data/environmental/ibutton_data/clean/")
-for(ii in length(file_names)) {
-  file_list[[ii]] <- read.csv(paste0("data/environmental/ibutton_data/clean/", file_names[ii]))
-}
